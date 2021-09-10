b059562ca1a441e4ed927a9c9b30afc3d80b93d1
steven278 <darkavenger347@gmail.com>
2021-09-10 21:17:04 +0700

    Fixing repo

---
+++ Week01_StevenLie_33747/Week01_33747/app/src/main/java/id/ac/umn/week01_33747/MainActivity.java
@@ -0,0 +1,15 @@
+package id.ac.umn.week01_33747;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
