b90f38a3ef6f325ad2719d6ed3927abfdc2f5d2b
steven278 <darkavenger347@gmail.com>
2021-09-13 12:36:52 +0700

    no change

--- Week04_StevenLie_33747/Week4/Week04B_33747/app/src/main/java/id/ac/umn/week04b_33747/MainActivity.java
+++ Week04_StevenLie_33747/Week4/Week04B_33747/app/src/main/java/id/ac/umn/week04b_33747/MainActivity.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button button_change_1, button_change_2;
+    private EditText nama;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- Week04_StevenLie_33747/Week4/Week04B_33747/app/src/main/java/id/ac/umn/week04b_33747/ThirdActivity.java
+++ Week04_StevenLie_33747/Week4/Week04B_33747/app/src/main/java/id/ac/umn/week04b_33747/ThirdActivity.java
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
