16c1fda89a8819b76409b850cdd8da7b436dd35a
steven278 <darkavenger347@gmail.com>
2021-09-21 12:18:57 +0700

    Finished Week6A & Create Week6B

---
+++ Week06_StevenLie_33747/Week06B_33747/app/src/main/java/id/ac/umn/week06b_33747/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.week06b_33747;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
