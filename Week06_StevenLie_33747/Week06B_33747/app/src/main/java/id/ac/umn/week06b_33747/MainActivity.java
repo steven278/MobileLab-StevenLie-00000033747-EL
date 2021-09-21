97afbc3c8781aad0a474cf41331beab8ad67b601
steven278 <darkavenger347@gmail.com>
2021-09-21 12:30:33 +0700

    Finished Week06B

--- Week06_StevenLie_33747/Week06B_33747/app/src/main/java/id/ac/umn/week06b_33747/MainActivity.java
+++ Week06_StevenLie_33747/Week06B_33747/app/src/main/java/id/ac/umn/week06b_33747/MainActivity.java
@@ -2,13 +2,26 @@ package id.ac.umn.week06b_33747;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    AnimationDrawable animasiKuda;
+    ImageView gambarKuda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gambarKuda = (ImageView) findViewById(R.id.gambarKuda);
+        gambarKuda.setBackgroundResource(R.drawable.kuda_lari);
+        animasiKuda = (AnimationDrawable) gambarKuda.getBackground();
+        gambarKuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animasiKuda.start();
+            }
+        });
     }
 }
