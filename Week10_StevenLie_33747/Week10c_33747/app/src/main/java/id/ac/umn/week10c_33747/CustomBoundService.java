999fbf77d27a251200608c72213f13ee7e9bbee1
steven278 <darkavenger347@gmail.com>
2021-10-19 11:30:37 +0700

    week 10 a b c done

---
+++ Week10_StevenLie_33747/Week10c_33747/app/src/main/java/id/ac/umn/week10c_33747/CustomBoundService.java
@@ -0,0 +1,33 @@
+package id.ac.umn.week10c_33747;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CustomBoundService extends Service {
+    public IBinder customBinder = new CustomLocalBinder();
+    public CustomBoundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return customBinder;
+    }
+
+    public class CustomLocalBinder extends Binder {
+        CustomBoundService getService(){
+            return CustomBoundService.this;
+        }
+    }
+    public String getCurrentTime(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US);
+        return (dateFormat.format(new Date()));
+    }
+}
